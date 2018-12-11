85d0aa9412407bb80b04cbb2a4bbdecc89d47b77
Jiri Brabec <brabeji@gmail.com>
2018-12-11 14:33:24 +0100

    Build for old browsers

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -4,6 +4,18 @@ module.exports = {
 			'@neutrinojs/library',
 			{
 				name: '@usertech/react-routing',
+				babel: {
+					// Override options for babel-preset-env
+					presets: [
+						['babel-preset-env', {
+							// Passing in browser targets to babel-preset-env will replace them
+							// instead of merging them when using the 'web' target
+							targets: {
+								browsers: ["last 2 versions", "ie >= 7"],
+							}
+						}]
+					]
+				}
 			},
 		],
 		(neutrino) => neutrino.config.resolve.modules.add(neutrino.options.source),
